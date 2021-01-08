326bd518721df29b2071628adb6c5f222fa9c94e
limZyuan <58379837+limZyuan@users.noreply.github.com>
2021-01-08 18:03:38 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+import axios from "axios";
+import "./App.css";
+
+const POSTS_PER_PAGE = 10;
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // componentdidmount
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * POSTS_PER_PAGE;
+  const indexOfFirstPost = indexOfLastPost - POSTS_PER_PAGE;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (number) => setCurrentPage(number);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={POSTS_PER_PAGE}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Posts = ({ posts, loading }) => {
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+
+  return (
+    <ul className="list-group mb-4">
+      {posts.map((post) => {
+        return (
+          <li className="list-group-item" key={post.id}>
+            {post.title}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Posts;
